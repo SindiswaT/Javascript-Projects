e0da0e0385f6f1c018551719ddeb0bd79a64d0bd
SindiswaT <sindiswatoli@gmail.com>
2022-11-06 20:09:24 +0200

    Password Generator

--- PW Generator/main.js
+++ PW Generator/main.js
@@ -1,23 +1,20 @@
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",
-"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", 
-"0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
-"~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",
-":",";","<",">",".","?",
-"/"];
+let password = document.getElementById("password")
 
-let passwordLength = 12
-let password = ""
+function genPassword(){
+  let passwordLength = 15
+  let password = ""
 
-document.getElementById("pwOne").value = password
-document.getElementById("pwTwo").value = password
-
-document.getElementById("btn-pw").addEventListener("click", generatePassword)
-
-function generatePassword(){
-  for(let i = 0; i < passwordLength; i++){
-    let randomPwOne = Math.floor(Math.random() * characters.length)
-    let randomPwTwo = Math.floor(Math.random() * characters.length)
-    password += characters.substring(randomPwOne, randomPwOne + 1, randomPwTwo, randomPwTwo + 1)
+  for(let i = 0; i <= passwordLength; i++){
+    let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789~,`,!@#$%^&*()_-+={[}]|:;<>.?/"
+    let randomNumber = Math.floor(Math.random() * characters.length)
+    password += characters.substring(randomNumber, randomNumber + 1)
   }
-       
+  document.getElementById("password").value = password
+}
+
+function copyPassword(){
+  let copyText =document.getElementById("password")
+  copyText.select()
+  copyText.setSelectionRange(0,999)
+  document.execCommand("copy")
 }
